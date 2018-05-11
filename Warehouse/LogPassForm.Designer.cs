8605daeecfeacc0e23f0d572059be9098897fe39
morozzzova <38218956+morozzzova@users.noreply.github.com>
2018-05-11 15:38:54 +0300

    LogPassForm

---
+++ Warehouse/LogPassForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Warehouse
+{
+    partial class LogPassForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null)
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.dataSet = new Warehouse.DataSet();
+            this.personTA = new Warehouse.DataSetTableAdapters.personTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Location = new System.Drawing.Point(12, 13);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(38, 13);
+            this.LoginLabel.TabIndex = 0;
+            this.LoginLabel.Text = "Логин";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(65, 10);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(125, 20);
+            this.Login.TabIndex = 1;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 43);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(45, 13);
+            this.PasswordLabel.TabIndex = 2;
+            this.PasswordLabel.Text = "Пароль";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(65, 40);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '•';
+            this.Password.Size = new System.Drawing.Size(125, 20);
+            this.Password.TabIndex = 3;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(65, 66);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 23);
+            this.EnterButton.TabIndex = 4;
+            this.EnterButton.Text = "Войти";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // dataSet
+            // 
+            this.dataSet.DataSetName = "DataSet";
+            this.dataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personTA
+            // 
+            this.personTA.ClearBeforeFill = true;
+            // 
+            // LogPassForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 96);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.LoginLabel);
+            this.Name = "LogPassForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вход";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button EnterButton;
+        private DataSet dataSet;
+        private DataSetTableAdapters.personTableAdapter personTA;
+    }
+}
